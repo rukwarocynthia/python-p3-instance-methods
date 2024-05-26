f911cc0f423c21ce11249b8a3eba8b270aca2feb
rukwarocynthia4093@gmail.com <rukwarocynthia4093@gmail.com>
2024-05-26 18:17:11 +0300

    done

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,13 @@ class Person:
     # Class body goes here
 
     #Instance method definition
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
     pass
+
+jim = Person()
+jim.talk()
+jim.walk()
